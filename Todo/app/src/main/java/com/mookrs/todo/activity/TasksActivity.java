e41668a74ddb64225a344a7718554db65f2bfde0
mookrs <mookrs@gmail.com>
2016-04-14 17:55:30 +0800

    add: Todo app. A project follow googlesample.

---
+++ Todo/app/src/main/java/com/mookrs/todo/activity/TasksActivity.java
@@ -0,0 +1,7 @@
+package com.mookrs.todo.activity;
+
+/**
+ * Created by mookrs on 16/4/14.
+ */
+public class TasksActivity {
+}
---
+++ Todo/app/src/main/java/com/mookrs/todo/fragment/TasksFragment.java
@@ -0,0 +1,7 @@
+package com.mookrs.todo.fragment;
+
+/**
+ * Created by mookrs on 16/4/14.
+ */
+public class TasksFragment {
+}
